31496c7d2186d242e16d501f8cff886fbf37f8e0
Luís Davi Gomes de Santana <101807467+daviStudentDS@users.noreply.github.com>
2023-03-13 09:21:18 -0300

    commit god

---
+++ Exemplo_AulaEXERCICIOS/Exemplo_AulaEXERCICIOS/Controllers/AulaController.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo_AulaEXERCICIOS.Controllers
+{
+    public class AulaController : Controller
+    {
+        // GET: Aula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aluno()
+        {
+            ViewBag.Primeira = "Aluno";
+            ViewData["Pri"] = "Os melhores alunos são aqueles que estudam mais.";
+            return View();
+        }
+        public ActionResult Teste()
+        {
+            TempData["Exemplo3"] = "";
+            return RedirectToAction("Aluno");
+        }
+
+        public ActionResult Resultado()
+        {
+            if (TempData["Exemplo3"] != null)
+                ViewBag.Exemplo2 = "Os melhores alunos são aqueles que estudam mais.";
+            return View();
+
+
+
+        }
+        public ActionResult Google()
+        { 
+            return Redirect("https://google.com");
+        }
+
+    }
+}
---
+++ Exemplo_AulaEXERCICIOS/Exemplo_AulaEXERCICIOS/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exemplo_AulaEXERCICIOS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            TempData["Exemplo3"] = "O MVC é um padrão muito utilizado no mundo web é conhecido por 'forçar' o programador a separar suas responsabilidades.";
+
+            return View();
+        }
+        
+    }
+}
